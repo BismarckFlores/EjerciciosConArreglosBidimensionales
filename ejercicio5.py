ccbb865b298736d45fd87f8ee4a0eb3a5d693ee0
Bismarck Flores <bjflores@uamv.edu.ni>
2025-05-25 23:03:25 -0600

    Inciso B completado - Se agrego la suma de ventas por vendedor, ademas de una limpieza de codigo menor

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,10 +1,10 @@
 ventas = [[50, 88, 30, 45], [9, 45, 30, 20], [59, 90, 10, 60]]
 zonas = ["zona 1", "Zona 2", "Zona 3", "Zona 4"]
 vendedores = ["Vendedor 1", "Vendedor 2", "Vendedor 3", "Vendedor 4"]
+
 num_vendedores = len(ventas)
 num_zonas = len(ventas[0])
 
-print(num_vendedores, num_zonas)
 
 # a) Suma de ventas por zona
 ventas_zona = []
@@ -14,7 +14,6 @@ for zona in range(num_zonas):
     for vendedor in range(num_vendedores):
         suma += ventas[vendedor][zona]
     ventas_zona.append(suma)
-print(ventas_zona)
 
 ## Encontrar la zona que vendio mas
 mas_ventas_zona = 0
@@ -27,4 +26,27 @@ for zona in range(num_zonas):
         mas_ventas_zona = venta
         zona_max = zona
 
+print(ventas_zona)
 print(f"La zona con mas ventas es la {zonas[zona_max]}, con una venta de {mas_ventas_zona} computadoras")
+
+
+# b) Sumar ventas por vendedor
+suma_vendedor = []
+
+for vendedor in range(num_vendedores):
+    suma = sum(ventas[vendedor])
+    suma_vendedor.append(suma)
+
+## Encontrar el vendedor que menos vendio
+min_ventas_vendedor = suma_vendedor[0]
+vendedor_min = 0
+venta = 0
+
+for vendedor in range(num_vendedores):
+    venta = suma_vendedor[vendedor]
+    if venta < min_ventas_vendedor:
+        min_ventas_vendedor = venta
+        vendedor_min = vendedor
+
+print(suma_vendedor)
+print(f"El vendedor con menos ventas fue el {vendedores[vendedor_min]}, con un total de {min_ventas_vendedor}")
